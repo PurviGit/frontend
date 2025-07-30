713d1539b7bb5e0a111fc1261db2ab1e0da25eee
pp <139757278+PurviGit@users.noreply.github.com>
2025-07-30 23:09:30 +0530

    Add files via upload
    frontend

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Devotion from "./pages/Devotion";
+import Prayer from "./pages/Prayer";
+import Meditation from "./pages/Meditation";
+import Accountability from "./pages/Accountability";
+import Settings from "./pages/Settings";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/devotion" element={<Devotion />} />
+          <Route path="/prayer" element={<Prayer />} />
+          <Route path="/meditation" element={<Meditation />} />
+          <Route path="/accountability" element={<Accountability />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
